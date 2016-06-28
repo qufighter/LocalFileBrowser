a51e8291b3e25c6f2d3a24538b1340684cc95d37
qufighter <qufighter@gmail.com>
2016-06-28 18:46:11 -0500

    update favicon to reflect current image

--- localfiles.user.js
+++ localfiles.user.js
@@ -998,6 +998,7 @@ function isViewingImage_LoadStylesheet(){
 
 function injectStyleSheet(){
   Cr.elm('link',{href:chrome.extension.getURL('localfiles_print.css'),rel:'stylesheet',type:'text/css',media:'print'},[],document.head);
+  Cr.elm('link', {id:'shortcutIcon', rel:"shortcut icon", href:directoryURL+startFileName, type:"image/x-icon"}, [], document.head);
 }
 
 function navToFileByElmName(ev){
@@ -1012,6 +1013,11 @@ function winLocGoCurrent(){
   window.location=directoryURL+startFileName;
 }
 
+function currentFile(){
+  return dirFiles[dirCurFile].file_name;
+}
+
+var shortcutTimeout = 0;
 function navToSrc(src,suppressPushState,loadedFileName){
 
   //this would be WAY better!  unfortunately:
@@ -1089,6 +1095,12 @@ function navToSrc(src,suppressPushState,loadedFileName){
     resumeAutoPlay();
     fetchNewDirectoryListing(true); // this will potentially pause auto play again!
     //don't do this every time! slows things down!
+
+    clearTimeout(shortcutTimeout);
+    shortcutTimeout = setTimeout(function(){
+      gel('shortcutIcon').href=src;
+    },100);
+
   }
   newimg.src=src;
 }
